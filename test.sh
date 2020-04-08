e5cc1dc1f3844d874d9c1541e741945345068afd
test <test@test.com>
2020-04-08 11:47:31 +0800

    openwrt 10

--- test.sh
+++ test.sh
@@ -10,11 +10,11 @@ git clone -b openwrt-19.07 https://github.com/openwrt/openwrt.git
 pushd openwrt
 
 echo download feed >> ../output/aaaa.txt
-#./scripts/feeds update -a
-#./scripts/feeds install -a
+./scripts/feeds update -a
+./scripts/feeds install -a
 
-#echo config >> ../output/aaaa.txt
-#make defconfig
+echo config >> ../output/aaaa.txt
+make defconfig
 popd
-#cp openwrt/.config output/
+cp openwrt/.config output/
 tree openwrt/ >> output/aaaa.txt
