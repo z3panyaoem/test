05b1742f3f3738986e241bd77acc127f1452a3c1
test <test@test.com>
2020-04-08 11:43:34 +0800

    openwrt 8

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ echo download src >> output/aaaa.txt
 git clone -b openwrt-19.07 https://github.com/openwrt/openwrt.git
 pushd openwrt
 
-#echo download feed >> ../output/aaaa.txt
+echo download feed >> ../output/aaaa.txt
 #./scripts/feeds update -a
 #./scripts/feeds install -a
 
