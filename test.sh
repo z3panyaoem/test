696db1daa461b37e9b82e38ffd7a6398b119f495
test <test@test.com>
2020-04-08 11:30:23 +0800

    openwrt 3

--- test.sh
+++ test.sh
@@ -4,14 +4,14 @@ echo start >> output/aaaa.txt
 
 echo download src >> output/aaaa.txt
 git clone -b openwrt-19.07 https://github.com/openwrt/openwrt.git
-pushd openwrt
+#pushd openwrt
 
-echo download feed >> ../output/aaaa.txt
-./scripts/feeds update -a
-./scripts/feeds install -a
+#echo download feed >> ../output/aaaa.txt
+#./scripts/feeds update -a
+#./scripts/feeds install -a
 
-echo config >> ../output/aaaa.txt
-make defconfig
-popd
-cp openwrt/.config output/
-tree openwrt/ >> output/aaaa.txt
+#echo config >> ../output/aaaa.txt
+#make defconfig
+#popd
+#cp openwrt/.config output/
+#tree openwrt/ >> output/aaaa.txt
