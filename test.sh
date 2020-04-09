d9a8b620decb293dc79216566506ddc136a6c3c2
kukutt <pan3yao@gmail.com>
2020-04-10 07:36:16 +0800

    test 4

--- test.sh
+++ test.sh
@@ -5,4 +5,7 @@ netstat -ant
 ping i.aganzai.com -c 20 
 ping www.baidu.com -c 20 
 ping www.aliyun.com -c 20 
+ping www.google.com -c 20 
+ping www.github.com -c 20 
+ping fq3.aganzai.com -c 20 
 echo aaaaaaaaaaaaaaaaaaaaa2
