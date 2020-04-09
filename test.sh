63252cad9dbcdcdb60b2a2c3a513a48fd9e06e55
kukutt <pan3yao@gmail.com>
2020-04-10 07:45:32 +0800

    test 5

--- test.sh
+++ test.sh
@@ -7,5 +7,6 @@ ping www.baidu.com -c 20
 ping www.aliyun.com -c 20 
 ping www.google.com -c 20 
 ping www.github.com -c 20 
-ping fq3.aganzai.com -c 20 
+ping fq3.aganzai.com -c 20
+echo "test" | nc -w1 i.aganzai.com 9999
 echo aaaaaaaaaaaaaaaaaaaaa2
